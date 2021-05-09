9c28452ce9bcc82dc749141a47eb9445576bbd50
“Israel <israelcoronel96@gmail.com>
2021-05-09 00:34:46 -0500

    codigo completo

---
+++ agenciadeviajes.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS `testimoniales`;
+
+CREATE TABLE `testimoniales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `mensaje` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `testimoniales` WRITE;
+
+UNLOCK TABLES;
+
+
+DROP TABLE IF EXISTS `viajes`;
+
+CREATE TABLE `viajes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(60) NOT NULL,
+  `precio` varchar(10) NOT NULL,
+  `fecha_ida` date NOT NULL,
+  `fecha_vuelta` date NOT NULL,
+  `imagen` varchar(20) NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `disponibles` varchar(3) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `viajes` WRITE;
